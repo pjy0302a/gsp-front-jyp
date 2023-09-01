31a54d488c2634c5fdac55a9505b83f4efa47e9c
pjy0302a <pjy0302a@naver.com>
2023-09-02 02:33:17 +0900

    20230902

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,10 +5,14 @@ function TodoList() {
     // 지정된 모든리스트를 보여준다. 
     // 할 것 인경우 time이 실시간으로 보여주고
     // 끝낼 것 인경우 time이 완료로 변경
+    clickStatus = () => {
+        console.log(this.target.text());
+    } 
+    // 
     return <div>
-        <button>할 것</button>
+        <button name='toStart' onClick={clickStatus()}>할 것</button>
         <img className="addButton" width="150px" height="50px" src="image/addButton.png"/>
-        <button>끝낼 것</button>
+        <button name='toEnd' onClick={clickStatus()}>끝낼 것</button>
         
     </div>
 }
